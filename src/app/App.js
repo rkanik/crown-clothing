390b46d0d765152772b6652487bbe51140d7d9b8
Md Rasel Khandkar <rasel15-7040@diu.edu.bd>
2020-01-24 22:17:03 +0600

    [Category grid added on Homepage]

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+import Categories from "../components/categories/categories"
+
+const App = () => {
+   return (
+      <div className='app'>
+         <Categories />
+      </div >
+   );
+}
+
+export default App;
